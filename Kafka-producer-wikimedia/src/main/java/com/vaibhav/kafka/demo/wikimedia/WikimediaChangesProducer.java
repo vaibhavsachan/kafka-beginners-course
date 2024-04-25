2f15c2e2a2983bfdd1267e9b095d3fc4ad470aba
Vaibhav Sachan <vaibbhav16@gmail.com>
2024-04-25 13:57:23 -0400

    first-commit

---
+++ Kafka-producer-wikimedia/src/main/java/com/vaibhav/kafka/demo/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,8 @@
+package com.vaibhav.kafka.demo.wikimedia;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) {
+        Employee e = new Employee(1, "Vaibhav");
+        System.out.println(e.printName());
+    }
+}
